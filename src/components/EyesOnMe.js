9641ee42929e66f549bb72b5db0ef1efcf9aa1f5
alexmpalermo <alexmpalermo@gmail.com>
2019-07-03 17:44:33 -0400

    Done.

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,22 @@
-// Code EyesOnMe Component Here
+import React from 'react';
+
+export default class EyesOnMe extends React.Component {
+
+  constructor() {
+      super();
+      this.handleFocus = this.keyUpHandler.bind(this, 'Good!');
+      this.handleBlur = this.keyUpHandler.bind(this, 'Hey! Eyes on me!');
+    }
+
+  keyUpHandler(refName, e) {
+        console.log(refName);
+    }
+
+  render() {
+     return (
+       <div>
+       <button onFocus={this.handleFocus} onBlur={this.handleBlur} >Button</button>
+       </div>
+     )
+   }
+}
--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,1 +1,21 @@
-// Code Keypad Component Here
+import React from 'react';
+
+export default class Keypad extends React.Component {
+
+  constructor() {
+      super();
+      this.handleLoginKeyUp = this.keyUpHandler.bind(this, 'Entering password...');
+    }
+
+  keyUpHandler(refName, e) {
+        console.log(refName);
+    }
+
+  render() {
+     return (
+       <div>
+       <input type="password" onKeyUp={this.handleLoginKeyUp} />
+       </div>
+     )
+   }
+}
