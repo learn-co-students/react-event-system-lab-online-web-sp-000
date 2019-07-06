5c113a5270f306ba1b81d7d54ade2420c2492827
github username <github email address>
2019-07-06 19:23:26 -0400

    Done.

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,14 @@
 // Code EyesOnMe Component Here
+// Code Keypad Component Here
+
+
+// export default class EyesOnMe extends Component {
+//
+//   //   return (
+//   //     // <div className="comment" >
+//   //     //    {this.props.commentText}
+//   //     // </div>
+//   //   )
+//   // }
+//
+// }
--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,1 +1,10 @@
 // Code Keypad Component Here
+import React, { Component } from 'react';
+
+export default class Keypad extends Component {
+  // render() {
+  //   // return (
+  //   //
+  //   // )
+  // }
+}
