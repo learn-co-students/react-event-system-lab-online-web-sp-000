b56d8f3710422ef70d5aef315f5f9873e8525075
Kate Follett <kate.follett@baruchmail.cuny.edu>
2020-04-21 19:39:42 -0400

    Done.

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-import Keypad from './components/Keypad';
-import EyesOnMe from './components/EyesOnMe';
+import { Keypad } from './components/Keypad';
+import { EyesOnMe }  from './components/EyesOnMe';
 
 ReactDOM.render(
   <div>
