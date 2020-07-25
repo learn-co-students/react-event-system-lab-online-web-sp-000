ebc7c696d3b5621d86c39087734fd0f93f4b8bee
Soner Mezgitci <mezgitci9@gmail.com>
2020-07-24 21:20:50 -0400

    Done.

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,14 @@
-// Code EyesOnMe Component Here
+import React from 'react'
+
+export default  class EyesOnMe extends React.Component {
+    render() {
+        return (
+        <div>
+            
+        </div>
+                
+            
+        )
+    }
+    
+}
