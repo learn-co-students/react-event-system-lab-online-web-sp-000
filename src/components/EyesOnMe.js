84f6b8dc5f31694c17e356fc0a5ec6ddd169f8e1
github username <github email address>
2020-09-16 15:11:32 -0500

    Done.

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,21 @@
-// Code EyesOnMe Component Here
+import React, {Component} from 'react';
+import { render } from 'enzyme';
+
+export default class EyesOnMe extends Component {
+
+    focus = () => {
+        console.log('Good!')
+    }
+
+    blur = () => {
+        console.log('Hey! Eyes on me!')
+    }
+    render () {
+        const txt = this.blur ? 'Hey! Eyes on me!' : 'Good!'
+return (
+    <div>
+<button onFocus={this.focus} onBlur = {this.blur}>{txt}</button>
+</div>  
+)
+    }
+}
