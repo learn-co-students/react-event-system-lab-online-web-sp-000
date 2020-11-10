3c2817c9031f92c6e774d3c73bb1c7507a3453f1
Will Carter <will.carter@fergusllc.com>
2020-11-10 13:43:34 -0700

    Done.

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,16 @@
-// Code EyesOnMe Component Here
+import React, { Component } from 'react';
+
+export default class EyesOnMe extends Component {
+
+    focus(event) {
+        console.log('Good!')
+    }
+
+    blur(event) {
+        console.log('Hey! Eyes on me!')
+    }
+
+    render() {
+        return <button onFocus={this.focus} onBlur={this.blur}>Log in</button>
+    } 
+}
