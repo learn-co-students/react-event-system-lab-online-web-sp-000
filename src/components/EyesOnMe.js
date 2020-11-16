9c342e6b586efb803eb9c95da6e1c648630dfc6d
connorworthen <connorw220@protonmail.com>
2020-11-15 20:52:10 -0800

    Done.

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,14 @@
+import React, { Component } from "react";
+
 // Code EyesOnMe Component Here
+export default class EyesOnMe extends Component {
+
+  focus = () => (console.log('Good!'))
+  blur = () => (console.log('Hey! Eyes on me!'))
+
+  render() {
+    return(
+      <button onFocus={this.focus} onBlur={this.blur} type="button" name="button"></button>
+    )
+  }
+}
