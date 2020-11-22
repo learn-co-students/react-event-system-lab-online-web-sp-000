6f9181b89927b6de3cb0d79cb98541268271aea4
David Strauch <dmaster18@gmail.com>
2020-11-22 03:29:56 +0000

    Done.

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,21 @@
-// Code EyesOnMe Component Here
+// Code Keypad Component Here
+import React, { Component } from 'react';
+
+class EyesOnMe extends Component {
+
+  good = () => {
+     console.log('Good!');
+   }
+
+   hey = () => {
+      console.log('Hey! Eyes on me!');
+    }
+
+  render() {
+    return (
+      <button onFocus={this.good} onBlur={this.hey}>Button</button>
+    )
+  }
+}
+
+export default EyesOnMe;
--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,1 +1,17 @@
 // Code Keypad Component Here
+import React, { Component } from 'react';
+
+class Keypad extends Component {
+
+  input = () => {
+     console.log('Entering password...');
+   }
+
+  render() {
+    return (
+      <input type="password" onKeyUp={this.input}>Enter password!</input>
+    )
+  }
+}
+
+export default Keypad;
