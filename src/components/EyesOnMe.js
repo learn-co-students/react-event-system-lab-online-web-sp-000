d23372f9581643cb70fa05f9811b07a5f88ba8fa
Yumacorn <ReachRyanMui@gmail.com>
2021-01-15 12:43:23 -0500

    Done.

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,15 @@
 // Code EyesOnMe Component Here
+import React, { Component} from 'react'
+
+export default class EyesOnMe extends Component {
+
+    // handleKeyUp = () => {
+    //     console.log('Entering password...')
+    // }
+
+    render() {
+        return (null
+        // <input type="password" keyUp={this.handleKeyUp} />
+        )
+    }
+}
