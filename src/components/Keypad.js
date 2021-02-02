2f30a801a1e07bbdfa079030137de103a8bd210e
jenleebeaver <jen.lee.beaver@gmail.com>
2021-02-02 10:03:55 +0530

    Done.

--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,1 +1,15 @@
 // Code Keypad Component Here
+import React, { Component } from 'react';
+
+export default class Keypad extends Component {
+
+    simulate = () => {
+        console.log('Entering password...');
+    }
+
+    render() {
+        return(
+            <input type="password" keyUp={this.simulate}>Enter password</input>
+        )
+    }
+}
