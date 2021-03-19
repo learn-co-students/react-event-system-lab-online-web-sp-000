2afa284b495f2656e44f2d1b3f4dc526b5ba5bed
keelsk <keelsk@gmail.com>
2021-03-18 21:48:45 -0400

    Done.

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,19 @@
 // Code EyesOnMe Component Here
+
+import React, { Component } from 'react';
+
+export default class EyesOnMe extends Component {
+    render(){
+        handleBlur = () => {
+            console.log('Hey! Eyes on me!')
+        }
+
+        handleFocus = () => {
+            console.log('Good!')
+        }
+
+        return(
+            <button onFocus={this.handleFocus} onBlur={this.handleBlur}></button>
+        )
+    }
+}
--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,1 +1,14 @@
 // Code Keypad Component Here
+import React, { Component } from 'react';
+
+export default class Keypad extends Component {
+    render(){
+        pwUpdate = () => {
+            console.log('Entering password...')
+        }
+
+        return(
+            <input onKeyUp={this.pwUpdate} type="password" />
+        )
+    }
+}
