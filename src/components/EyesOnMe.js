db445683c752e02124488dd7df5e254053edc74a
Austin Rhoads <austinrrhoads@ymail.com>
2021-04-22 15:29:45 -0500

    fish have wishes

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,30 @@
 // Code EyesOnMe Component Here
+import React from 'react'
+
+
+export default class EyesOnMe extends React.Component{
+
+  //  constructor(props){
+  //      super(props)
+  //  }
+
+  onBlurHandler = () => {
+    console.log('Hey! Eyes on me!')
+  }
+
+  onFocusHandler = () =>{
+      console.log('Good!')
+  }
+
+
+render(){
+    return(
+        <button className="EyesOnMe" onBlur={this.onBlurHandler} onFocus={this.onFocusHandler}>
+
+        </button>
+    )
+}
+
+
+
+}
--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,1 +1,21 @@
 // Code Keypad Component Here
+import React from 'react'
+
+
+export default class Keypad extends React.Component{
+//    constructor(props){
+//        super(props)
+//    }
+
+    keyUpHandler = () =>{
+        console.log("Entering password...")
+    }
+
+
+    render(){
+        return(
+       <input className="keypad" type="password" onKeyUp={this.keyUpHandler}></input>
+        )
+    }
+
+}
