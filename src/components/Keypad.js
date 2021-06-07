dfd4546377bc07e329a70dbac3bf9fc5909fb580
yarvinh <yarvinhhernandez@gmail.com>
2021-06-07 19:37:58 -0400

    Done.

--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,1 +1,29 @@
 // Code Keypad Component Here
+import React, { Component } from 'react';
+
+export default class Keypad extends Component {
+    constructor(props) {
+        super()
+        this.state = {
+          
+        } 
+      }
+      onKeyUp(event) {
+          console.log('Entering password...')
+        // if (event.charCode === 13) {
+        //   this.setState({ inputValue: event.target.value });
+        // }
+      }
+
+
+      render() {
+        return (
+          <div>
+             <input onKeyUp={this.onKeyUp} type="password" />
+          </div>
+        )
+      }
+      
+
+  
+}
